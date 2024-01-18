7b0f9f4d3ee8829b8efa7409c3901c2a491582c2
Mazgaj <>
2024-01-18 22:13:05 +0100

    Project : Refactor | Change title and remove redundant files

--- src/components/Nav/MainNav.js
+++ src/components/Nav/MainNav.js
@@ -9,7 +9,7 @@ function MainNav() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Design Patterns</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
